85469a67fc532d08b96039c24673757b5fe6a218
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-08-22 08:12:14 +0200

    Preparation for merging

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,45 +9,48 @@
  * Main module of the application.
  */
 angular
-.module('projectReviewApp', ['ui.router','ui.select','angular.filter','hbpCommon','bbpOidcClient','ui.bootstrap','ngMaterial', 'ngMessages'])
+.module('proposalReviewApp', ['ui.router','ui.select','angular.filter','hbpCommon','bbpOidcClient','ui.bootstrap','ngMaterial', 'ngMessages'])
 .config(function ($stateProvider, $urlRouterProvider) {
 	// link adresses to views and controllers
 	$stateProvider
-  .state('form', {
-    templateUrl: 'views/form.html',
-    controller:'formController'
-  })
-  .state('form.review', {
-		url: '/',
-		templateUrl: 'views/review.html'
+	.state('main', {
+	  templateUrl: 'views/form.html',
+		controller: 'MainController'
 	})
-  .state('form.reviewing', {
+  .state('main.reviewApp', {
+    controller:'reviewAppController'
+  })
+  .state('main.reviewApp.review', {
+		url: '/review',
+		templateUrl: 'views/review/review.html'
+	})
+  .state('main.reviewApp.reviewing', {
 		url: '/review/:param',
-		templateUrl: 'views/reviewing.html',
+		templateUrl: 'views/review/reviewing.html',
 		controller:'ReviewCtrl'
 	})
-  .state('form.proposal', {
+  .state('main.reviewApp.proposal', {
 		url: '/review/:param/proposal',
-		templateUrl: 'views/proposal.html',
+		templateUrl: 'views/review/proposal.html',
     controller:'ProposalCtrl'
 	})
-  .state('form.note', {
+  .state('main.reviewApp.note', {
 		url: '/review/:param/note',
-		templateUrl: 'views/note.html'
+		templateUrl: 'views/review/note.html'
 	})
   .state('summary', {
 		url: '/review/:param/summary',
-		templateUrl: 'views/summary.html'
+		templateUrl: 'views/review/summary.html'
 	});
 	$urlRouterProvider.otherwise('/');
 })
 
-.controller('formController', function ($scope) {
+.controller('reviewAppController', function ($scope) {
 	$scope.data= {};
   $scope.data.select = {};
 	$scope.formData = {};
 
-  // when landing on the page, get all the proposals and show them
+  // when landing on the page, get all the projects and show them
   $http.get('/api/projects')
     .success(function(data) {
       $scope.data.projects = data;
