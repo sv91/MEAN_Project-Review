92fb0e28bf285af484f55c3c352701df3cf67d1d
Alexander Vostriakov <alexander.vostriakov@epfl.ch>
2016-10-24 15:19:32 +0200

    Deliverables comments

--- public/scripts/controllers/review/proposal.js
+++ public/scripts/controllers/review/proposal.js
@@ -199,5 +199,156 @@ getAllByID($scope.data.select.proposal.teams,'teams')
 });
 
   $scope.data.select.loaded = true;
+  $scope.data.summ = {};
+  $scope.data.summ.cloud = {};
+  $scope.data.summ.hpc = {};
+  $scope.data.summ.deliverables = [];
+
+  angular.forEach($scope.data.select.proposal.deliverables,function(val){
+    var temp ={};
+    temp.softdev=[];
+    temp.datatransfer=[];
+    temp.virtualization=[];
+    temp.devenv=[];
+    temp.collabs=[];
+    temp.architecture=[];
+    temp.architectureC=[];
+    temp.hpc={'runs':0,'partition':0,'time':0,'numarte':0,'sizearte':0};
+    temp.cloud={'runs':0,'partition':0,'time':0,'numarte':0,'sizearte':0};
+    temp.hardware=[];
+    temp.members=[];
+    temp.name=val.name;
+    temp.date=val.date;
+    temp.risks=val.risks;
+    temp.desc=val.description;
+    temp.dependency=val.dependency;
+
+
+    $scope.data.summ.hpcRessource = ($scope.data.summ.hpcRessource || val.hpcRessource);
+    temp.hpcRessource = val.hpcRessource;
+    $scope.data.summ.cloudRessource = ($scope.data.summ.cloudRessource || val.cloudRessource);
+    temp.cloudRessource = val.cloudRessource;
+
+    /**
+    * @ngdoc function
+    * @name add
+    * @description
+    * # add
+    * Verify if a value is present in the two arrays and if not add it into it.
+    * @param {Object} origin The object to add.
+    * @param {Object} summ First array.
+    * @param {Object} tempo Second array.
+    */
+    function add(origin,summ,tempo){
+      angular.forEach(origin,function(val2){
+        if (summ.indexOf(val2)==-1){
+          summ.push(val2);
+        }
+        if (tempo.indexOf(val2)==-1){
+          tempo.push(val2);
+        }
+      })
+    }
+
+    /**
+    * @ngdoc function
+    * @name add
+    * @description
+    * # add
+    * Verify if a value is present in the two arrays and if not add it into it.
+    * @param {Object} origin The object to add.
+    * @param {Object} summ First array.
+    * @param {Object} tempo Second array.
+    */
+    function add(origin,summ,tempo){
+      angular.forEach(origin,function(val2){
+        if (summ.indexOf(val2)==-1){
+          summ.push(val2);
+        }
+        if (tempo.indexOf(val2)==-1){
+          tempo.push(val2);
+        }
+      })
+    }
+
+    // Filling the arrays
+    add(val.softdev,$scope.data.summ.softdev,temp.softdev);
+    add(val.datatransfer,$scope.data.summ.datatransfer,temp.datatransfer);
+    add(val.virtualization,$scope.data.summ.virtualization,temp.virtualization);
+    add(val.devenv,$scope.data.summ.devenv,temp.devenv);
+    add(val.hardware,$scope.data.summ.hardware,temp.hardware);
+    add(val.collabs,$scope.data.summ.collabs,temp.collabs);
+
+    // Filling the members arrays
+    angular.forEach(val.members,function(val2){
+      var tempM = {'name':'','pm':''};
+      tempM.name=val2.name;
+      tempM.pm=val2.pm;
+      var index = findMember(val2.name,$scope.data.summ.members);
+      if (index==-1){
+        $scope.data.summ.members.push(tempM);
+      } else {
+        $scope.data.summ.members[index].pm = parseInt($scope.data.summ.members[index].pm) + parseInt(val2.pm);
+      }
+      var indexD = findMember(val2.name,temp.members);
+      if (indexD==-1){
+        temp.members.push(tempM);
+      } else {
+        temp.members[index].pm = parseInt(temp.members[index].pm) + parseInt(val2.pm);
+      }
+    })
+
+    // Filling the HPC arrays
+    angular.forEach(val.hpc,function(val2){
+      if ($scope.data.summ.architecture.indexOf(val2.type)==-1){
+        $scope.data.summ.architecture.push(val2.type);
+      }
+      $scope.data.summ.hpc.runs+= addIfNotNull(val2.runs);
+      $scope.data.summ.hpc.partition+= addIfNotNull(val2.part);
+      $scope.data.summ.hpc.time+= addIfNotNull(val2.time);
+      $scope.data.summ.hpc.numarte+= addIfNotNull(val2.arte);
+      $scope.data.summ.hpc.sizearte+= addIfNotNull(val2.size)*addIfNotNull(val2.arte);
+
+      if (temp.architecture.indexOf(val2.type)==-1){
+        temp.architecture.push(val2.type);
+      }
+      temp.hpc.runs+= addIfNotNull(val2.runs);
+      temp.hpc.partition+= addIfNotNull(val2.part);
+      temp.hpc.time+= addIfNotNull(val2.time);
+      temp.hpc.numarte+= addIfNotNull(val2.arte);
+      temp.hpc.sizearte+= addIfNotNull(val2.size)*addIfNotNull(val2.arte);
+    })
+
+    // Filling the Cloud arrays
+    angular.forEach(val.cloud,function(val2){
+      if ($scope.data.summ.architectureC.indexOf(val2.type)==-1){
+        $scope.data.summ.architectureC.push(val2.type);
+      }
+      $scope.data.summ.cloud.runs+= addIfNotNull(val2.runs);
+      $scope.data.summ.cloud.partition+= addIfNotNull(val2.part);
+      $scope.data.summ.cloud.time+= addIfNotNull(val2.time);
+      $scope.data.summ.cloud.numarte+= addIfNotNull(val2.arte);
+      $scope.data.summ.cloud.sizearte+= addIfNotNull(val2.size)*addIfNotNull(val2.arte);
+
+      if (temp.architectureC.indexOf(val2.type)==-1){
+        temp.architectureC.push(val2.type);
+      }
+      temp.cloud.runs+= addIfNotNull(val2.runs);
+      temp.cloud.partition+= addIfNotNull(val2.part);
+      temp.cloud.time+= addIfNotNull(val2.time);
+      temp.cloud.numarte+= addIfNotNull(val2.arte);
+      temp.cloud.sizearte+= addIfNotNull(val2.size)*addIfNotNull(val2.arte);
+    })
+
+    //Compute the averages for the deliverables
+    temp.cloud.sizearte = temp.cloud.sizearte/temp.cloud.numarte;
+    temp.hpc.sizearte = temp.hpc.sizearte/temp.hpc.numarte;
+    $scope.data.summ.deliverables.push(temp);
+  });
+
+  //Compute the averages for the project
+  $scope.data.summ.cloud.sizearte = $scope.data.summ.cloud.sizearte/$scope.data.summ.cloud.numarte;
+  $scope.data.summ.hpc.sizearte = $scope.data.summ.hpc.sizearte/$scope.data.summ.hpc.numarte;
+
 
 });
